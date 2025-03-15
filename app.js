25dcc4efc18148c418b1273ba8d58c2afc851f88
Gabriel de Oliveira Lima <biellima.oliver28@gmail.com>
2025-03-15 04:01:55 -0300

    Efeitos

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     // Restaura o texto original quando o mouse sai
-    devName.addEventListener("mouseenter", function () {
+    devName.addEventListener("mouseleave", function () {
         devName.textContent = originalText; // Restaura o texto original
     });
 });
@@ -22,27 +22,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Muda a cor do rodapé ao passar o mouse
     devName.addEventListener("mouseenter", function () {
-        rodape.style.backgroundColor = "#000000"; // Altere para a cor desejada
+        rodape.style.backgroundColor = "#F6F6F6"; // Altere para a cor desejada
     });
 
     // Restaura a cor original quando o mouse sai
-    devName.addEventListener("mouseenter", function () {
+    devName.addEventListener("mouseleave", function () {
         rodape.style.backgroundColor = "#22D4FD"; // Restaura a cor original
     });
 });
 
-
-
-        // Seleciona todos os links na navegação
-        const links = document.querySelectorAll('.apresentacao__links__link a');
-
-        // Adiciona um evento de clique a cada link
-        links.forEach(link => {
-            link.addEventListener('click', function(event) {
-                // Previne a navegação imediata
-                event.preventDefault();
-
-                // Reproduz o som
-                document.getElementById('meuSom').play();
-            });
-        });
