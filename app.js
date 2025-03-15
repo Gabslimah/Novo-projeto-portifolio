9a9782baa5566a604124d563b245b0caaa45bc6e
Gabriel de Oliveira Lima <biellima.oliver28@gmail.com>
2025-03-15 01:04:19 -0300

    Ajustes

--- app.js
+++ app.js
@@ -5,27 +5,30 @@ document.addEventListener("DOMContentLoaded", function () {
     const originalText = devName.textContent;
 
     // Muda o texto ao passar o mouse
-    devName.addEventListener("mouseover", function () {
-        devName.textContent = "Alura Projeto-ONE G8: Créditos Luri"; // Altere para o texto que você deseja exibir
+    devName.addEventListener("mouseenter", function () {
+        devName.textContent = "Alura Projeto-ONE G8: Créditos Luri"; // Altere para o texto que você deseja exibir //
     });
 
     // Restaura o texto original quando o mouse sai
-    devName.addEventListener("mouseout", function () {
+    devName.addEventListener("mouseenter", function () {
         devName.textContent = originalText; // Restaura o texto original
     });
 });
+
 
 document.addEventListener("DOMContentLoaded", function () {
     const devName = document.getElementById("dev-name");
     const rodape = document.querySelector(".rodape");
 
     // Muda a cor do rodapé ao passar o mouse
-    devName.addEventListener("mouseover", function () {
-        rodape.style.backgroundColor = "#22D4FD"; // Altere para a cor desejada
+    devName.addEventListener("mouseenter", function () {
+        rodape.style.backgroundColor = "#000000"; // Altere para a cor desejada
     });
 
     // Restaura a cor original quando o mouse sai
-    devName.addEventListener("mouseout", function () {
+    devName.addEventListener("mouseleave", function () {
         rodape.style.backgroundColor = "#22D4FD"; // Restaura a cor original
     });
 });
+
+
