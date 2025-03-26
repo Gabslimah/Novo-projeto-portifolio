7ca5a6fb8af1bc83482be82453e98775ee8f3351
Gabriel de Oliveira Lima <biellima.oliver28@gmail.com>
2025-03-26 05:21:25 -0300

    testanto mais resposividae

--- app.js
+++ app.js
@@ -1,39 +1,3 @@
-/* document.addEventListener("DOMContentLoaded", function () {
-    const devName = document.getElementById("dev-name");
-    const rodape = document.querySelector(".rodape");
-    // Armazena o texto original
-    const originalText = devName.textContent;
-
-    // Muda o texto ao passar o mouse
-    devName.addEventListener("mouseenter", function () {
-        devName.textContent = "©2025 Alura Projeto-ONE G8: Créditos Luri"; // Altere para o texto que você deseja exibir //
-        rodape.style.backgroundColor = "#F6F6F6"; // Altere para a cor desejada
-    });
-
-    // Restaura o texto original quando o mouse sai
-    devName.addEventListener("mouseleave", function () {
-        devName.textContent = originalText; // Restaura o texto original
-        rodape.style.backgroundColor = "#22D4FD";
-    });
-});
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const devName = document.getElementById("dev-name");
-    const rodape = document.querySelector(".rodape");
-
-    // Muda a cor do rodapé ao passar o mouse
-    rodape.addEventListener("mouseenter", function () {
-        rodape.style.backgroundColor = "#F6F6F6"; // Altere para a cor desejada
-        
-
-    // Restaura a cor original quando o mouse sai
-    rodape.addEventListener("mouseleave", function () {
-        rodape.style.backgroundColor = "#22D4FD"; // Restaura a cor original
-        
-    });
-});  */
-
 document.addEventListener("DOMContentLoaded", function () {
     const devName = document.getElementById("dev-name");
     const rodape = document.querySelector(".rodape");
@@ -53,28 +17,6 @@ document.addEventListener("DOMContentLoaded", function () {
         rodape.style.backgroundColor = "#22D4FD"; // Restaura a cor original
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
