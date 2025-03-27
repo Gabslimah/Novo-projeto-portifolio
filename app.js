2f8afe9472c420170c30a987fde9dcc9f31cf707
Gabriel de Oliveira Lima <biellima.oliver28@gmail.com>
2025-03-27 03:04:17 -0300

    alinhando icones

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ document.addEventListener("DOMContentLoaded", function () {
     // Armazena o texto original
     const originalText = devName.textContent;
 
-    // Muda o texto e a cor do rodapé ao passar o mouse
+    // Muda o texto e a cor ao passar o mouse (mouseenter)
     rodape.addEventListener("mouseenter", function () {
         devName.textContent = "©2025 Alura Projeto-ONE G8: Créditos Luri"; // Texto alternativo
         rodape.style.backgroundColor = "#F6F6F6"; // Altere para a cor desejada
     });
 
-    // Restaura o texto original e a cor quando o mouse sai
+    // Restaura o texto e a cor ao sair o mouse (mouseleave)
     rodape.addEventListener("mouseleave", function () {
         devName.textContent = originalText; // Restaura o texto original
         rodape.style.backgroundColor = "#22D4FD"; // Restaura a cor original
@@ -19,8 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-
-
+                   //Opção de código para colocar som nos botões //
 /* const links = document.querySelectorAll('a'); // Seleciona todos os links
 const clickSound = document.getElementById('click-sound'); // Seleciona o elemento de áudio
 let isPlaying = false;
@@ -43,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     links.forEach(link => {
         link.addEventListener('click', () => {
-            const clickSound = new Audio('som.mp3'); // Cria um novo objeto de áudio
+            const clickSound = new Audio('./sound/som.mp3'); // Cria um novo objeto de áudio
             clickSound.currentTime = 0; // Reinicia o som
             clickSound.play(); // Reproduz o som
         });
